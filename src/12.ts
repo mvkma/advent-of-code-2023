8e85ef6f81ba9645ea311eda906fbf6a6b81cd00
Matthias Volk <mv@infrablue.org>
2023-12-27 16:43:56 +0100

    Day 12, part 2

--- src/12.ts
+++ src/12.ts
@@ -1,3 +1,4 @@
+import { countReset } from "node:console";
 import * as fs from "node:fs/promises"
 
 const EXAMPLE_01 = `
@@ -47,20 +48,15 @@ function isComplete(record: string): boolean {
     return record.indexOf("?") === -1;
 }
 
-function getArrangements(line: string): string[] {
-    const parts = line.split(" ");
-
-    const sizes = parts[1].trim().split(",").map(Number);
-    const record = parts[0];
-
-    let arrangements = [];
+function countArrangements(sizes: number[], record: string): number {
+    let count = 0;
     let queue = [record];
 
     while (queue.length > 0) {
         let curr = queue.pop()!;
 
         if (isComplete(curr)) {
-            arrangements.push(curr);
+            count++;
             continue;
         }
 
@@ -72,7 +68,61 @@ function getArrangements(line: string): string[] {
         }
     }
 
-    return arrangements;
+    return count;
+}
+
+let _cache = new Map<string, number>();
+
+function countArrangementsRecursive(sizes: number[], record: string): number {
+    let key = sizes.toString() + record;
+    let val = 0;
+
+    if (_cache.has(key)) {
+        return _cache.get(key)!;
+    }
+
+    if (sizes.length === 0) {
+        val = record.indexOf("#") !== -1 ? 0 : 1;
+        _cache.set(key, val);
+        return val;
+    }
+
+    if (record.length === 0) {
+        let val = sizes.length !== 0 ? 0 : 1;
+        _cache.set(key, val);
+        return val;
+    }
+
+    let count = 0;
+
+    if (record[0] === "." || record[0] === "?") {
+        count += countArrangementsRecursive(sizes, record.slice(1));
+    }
+
+    if (record[0] === "#" || record[0] === "?") {
+        if (sizes[0] <= record.length &&
+            record.slice(0, sizes[0]).indexOf(".") === -1 &&
+            (sizes[0] === record.length || record[sizes[0]] !== "#")) {
+            count += countArrangementsRecursive(sizes.slice(1), record.slice(sizes[0] + 1));
+        }
+    }
+
+    _cache.set(key, count);
+    return count;
+}
+
+function getArrangements(line: string, repeat: number = 1): number {
+    const parts = line.split(" ");
+
+    let sizes = parts[1].trim().split(",").map(Number);
+    let record = parts[0];
+
+    if (repeat > 1) {
+        sizes = (new Array(repeat)).fill(sizes).reduce((a, b) => a.concat(b));
+        record = (new Array(repeat)).fill(record).reduce((a, b) => a + "?" + b);
+    }
+
+    return countArrangementsRecursive(sizes, record);
 }
 
 export async function main12() {
@@ -81,8 +131,7 @@ export async function main12() {
     let total = 0;
     // for (const line of EXAMPLE_01.split("\n").slice(1)) {
     for await (const line of file.readLines()) {
-        let arrangements = getArrangements(line);
-        total += arrangements.length;
+        total += getArrangements(line, 5);
     }
     console.log(total);
 }
