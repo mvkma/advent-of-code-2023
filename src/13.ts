11ccd4c6e47de7ee570a538728c7651c1df9bfde
Matthias Volk <mv@infrablue.org>
2023-12-28 12:10:17 +0100

    Day 13, part 2

--- src/13.ts
+++ src/13.ts
@@ -17,6 +17,45 @@ const EXAMPLE_01 = `
 ..##..###
 #....#..#`
 
+const EXAMPLE_02 = `
+#.#.#.###
+.#....#..
+..####..#
+#.###..##
+.#.##..##
+.#....###
+.#....###`
+
+const EXAMPLE_03 = `
+##.#....#
+...##...#
+...##..##
+##.......
+.....##..
+..#..##..
+..##....#
+..#..##..
+####....#`
+
+const EXAMPLE_04 = `
+#......####...##.
+#.####.#.....#.##
+#########.#..#..#
+#..##..#...##..##
+#..##..#...##..##
+#########.#..#..#
+#.####.#.....#.##
+#......####...##.
+#..##..#..#.##.#.
+.##..##.##.##..##
+..#..#..###.#####
+#.####.#.###..#..
+########.#####.##
+##.##.##....####.
+........###...#.#
+#..##..#..#.#.###
+###.####.####.###`
+
 function transpose(pattern: string[]): string[] {
     let transposed = [];
     let line = "";
@@ -32,8 +71,8 @@ function transpose(pattern: string[]): string[] {
     return transposed;
 }
 
-function findHorizontalReflection(pattern: string[]): number {
-    for (let i = 0; i < pattern.length; i++) {
+function findHorizontalReflection(pattern: string[], offset: number = 0): number {
+    for (let i = offset + 1; i < pattern.length; i++) {
         let j = 1;
         while (pattern[i + j - 1] === pattern[i - j]) {
             if (i - j === 0 || i + j - 1 === pattern.length - 1) {
@@ -45,22 +84,64 @@ function findHorizontalReflection(pattern: string[]): number {
     return -1;
 }
 
-function reflectionScore(pattern: string[]): number {
+function reflectionScore(pattern: string[], originalScore: number = -1): number {
     if (pattern.length === 0) {
         return 0;
     }
 
     let r = findHorizontalReflection(pattern);
+    if (100 * r === originalScore) {
+        r = findHorizontalReflection(pattern, originalScore % 100 === 0 ? originalScore / 100 : originalScore);
+    }
+
     if (r !== -1) {
         return 100 * r;
     }
 
     r = findHorizontalReflection(transpose(pattern));
-    if (r !== -1) {
+    if (r === originalScore) {
+        r = findHorizontalReflection(transpose(pattern), originalScore % 100 === 0 ? originalScore / 100 : originalScore);
+    }
+
+    if (r !== -1 && r !== originalScore) {
         return r;
     }
 
     return 0;
+}
+
+function fixSmudge(pattern: string[], originalScore: number) {
+    let copy = pattern.slice();
+
+    for (let i = 0; i < copy.length; i++) {
+        if (i > 0) {
+            copy[i - 1] = pattern[i - 1];
+        }
+
+        for (let j = 0; j < copy[0].length; j++) {
+            let c = copy[i][j];
+
+            switch (c) {
+                case ".": {
+                    copy[i] = copy[i].slice(0, j) + "#" + copy[i].slice(j + 1);
+                    break;
+                }
+                case "#": {
+                    copy[i] = copy[i].slice(0, j) + "." + copy[i].slice(j + 1);
+                    break;
+                }
+            }
+
+            let r = reflectionScore(copy, originalScore);
+            if (r !== originalScore && r !== 0) {
+                return r;
+            }
+
+            copy[i] = copy[i].slice(0, j) + c + copy[i].slice(j + 1);
+        }
+    }
+
+    return originalScore;
 }
 
 export async function main13() {
@@ -68,11 +149,20 @@ export async function main13() {
 
     let pattern = [];
     let total = 0;
+    let smearedTotal = 0;
+    let score = 0;
+    let smearedScore = 0;
 
-    // for (const line of EXAMPLE_01.split("\n")) {
+    // for (const line of EXAMPLE_02.split("\n")) {
     for await (const line of file.readLines()) {
+        if (line === "" && pattern.length > 0) {
+            score = reflectionScore(pattern);
+            total += score;
+            smearedScore = fixSmudge(pattern, score);
+            smearedTotal += smearedScore;
+        }
+
         if (line === "") {
-            total += reflectionScore(pattern);
             pattern = [];
             continue;
         }
@@ -80,6 +170,10 @@ export async function main13() {
         pattern.push(line);
     }
 
-    total += reflectionScore(pattern);
+    score = reflectionScore(pattern);
+    total += score;
+    smearedScore = fixSmudge(pattern, score);
+    smearedTotal += smearedScore;
     console.log(total);
+    console.log(smearedTotal);
 }
